251143fb4a22628c2fc18da3571f757473a0d4cd
Pof Magicfingers <pof@pof.pm>
2017-10-01 01:38:22 +0200

    removing double nl

--- test/test.js
+++ test/test.js
@@ -1,7 +1,6 @@
 var assert = require('assert');
 var server = require("../server.js")
 
-
 describe('Array', function() {
   describe('#indexOf()', function() {
     it('should return -1 when the value is not present', function() {
