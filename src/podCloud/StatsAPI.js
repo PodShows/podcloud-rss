153766999633d4acad53c5e67d4b1b5c4595dd50
Pof Magicfingers <pof@podshows.fr>
2019-07-03 16:56:40 +0200

    feat: filter requests from same IP/UA in analytics

--- src/podCloud/StatsAPI.js
+++ src/podCloud/StatsAPI.js
@@ -4,6 +4,20 @@ import { PodcastViewAppeal } from "podcloud-stats"
 
 import ua from "universal-analytics"
 
+import cacheManager from "cache-manager"
+
+import fsStore from "cache-manager-fs-binary"
+
+const diskCache = cacheManager.caching({
+  store: fsStore,
+  options: {
+    path: "diskcache", // path for cached files
+    ttl: 24 * 60 * 60, // time to life in seconds
+    maxsize: 1000 * 1000 * 1000 /* max size in bytes on disk */,
+    preventfill: true
+  }
+})
+
 export class podCloudStatsAPI {
   constructor() {}
 
@@ -46,15 +60,23 @@ export class podCloudStatsAPI {
 
       PodcastViewAppeal.process(payload).then(resolve, reject)
 
-      ua("UA-59716320-1")
-        .pageview({
-          dl: podcast.feed_url,
-          ua: payload.ua,
-          uip: payload.ip,
-          dr: payload.ref,
-          dt: podcast.title
-        })
-        .send()
+      diskCache.wrap(
+        `rss-${payload.fid}-${payload.ip}-${payload.ua}`,
+        () => {
+          console.log("Sent to GA")
+          ua("UA-59716320-1")
+            .pageview({
+              dl: podcast.feed_url,
+              ua: payload.ua,
+              uip: payload.ip,
+              dr: payload.ref,
+              dt: podcast.title
+            })
+            .send()
+        },
+        { ttl: 24 * 60 * 60 },
+        err => console.error(err)
+      )
     })
   }
 }
